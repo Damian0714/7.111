d25d7ad66f0624bd414ac97d2d698f44f72c9c20
Damian0714 <113889129+Damian0714@users.noreply.github.com>
2023-02-13 14:28:11 +0300

    Add files via upload

---
+++ 7.111/7.111/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] kar = new int[255];
+int[] moto = new int[255];
+double k = 0;
+double o = 0;
+int p, i;
+
+
+Console.WriteLine("Введите количество Машин и Мотоциклов: ");
+p = Convert.ToInt32(Console.ReadLine());
+for (i = 1; i <= p; i++)
+{
+    Console.WriteLine("Введите стоимость " + i + "й" + " машины: " +
+        "");
+    kar[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (i = 1; i <= p; i++)
+{
+    Console.WriteLine("Введите стоимость " + i + "" + " мотоцикла: ");
+    moto[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (i = 1; i < p; i++)
+{
+    o = o + kar[i];
+    k = k + moto[i];
+}
+o = o / p;
+k = k / n;
+if ((a / 3) > k)
+{
+    Console.WriteLine("Верно!");
+}
+else
+{
+    Console.WriteLine("Не верно!");
+
+}
